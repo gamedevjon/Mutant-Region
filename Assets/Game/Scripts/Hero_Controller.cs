ac906980408c0805888481bbdf5e0785c2344612
gamedevjon <jon@gamedevhq.com>
2023-05-24 23:43:37 -1000

    Updated controller movement. More efficient

--- Assets/Game/Scripts/Hero_Controller.cs
+++ Assets/Game/Scripts/Hero_Controller.cs
@@ -9,25 +9,10 @@ public class Hero_Controller : MonoBehaviour
     [SerializeField]
     private Animator _anim;
 
-    private enum DirectionToFace
-    {
-        N = 180,
-        NE = 135,
-        E = 90,
-        SE = 45, 
-        S = 0,
-        SW = -45,
-        W = -90,
-        NW = -135
-    }
-
-    private DirectionToFace _directionToFace = DirectionToFace.S;
-
     private void Start()
     {
         if (_anim == null)
             Debug.LogError("Animator Component Not Assigned");
-
        
     }
 
@@ -40,37 +25,11 @@ public class Hero_Controller : MonoBehaviour
 
         _anim.SetFloat("Move", Mathf.Abs(direction.magnitude));
 
-        //what direction do we face?
-        var currentRot = transform.localEulerAngles;
+        float angle = Mathf.Atan2(v, h) * Mathf.Rad2Deg;
 
-        //check north
-        if (v > 0 && h == 0)
-            _directionToFace = DirectionToFace.N;
-        //check north east
-        else if (v > 0 && h > 0)
-            _directionToFace = DirectionToFace.NE;
-        //check east
-        else if (v == 0 && h > 0)
-            _directionToFace = DirectionToFace.E;
-        //check South east
-        else if (v < 0 && h > 0)
-            _directionToFace = DirectionToFace.SE;
-        //check south
-        else if (v < 0 && h == 0)
-            _directionToFace = DirectionToFace.S;
-        //check south west
-        else if (v < 0 && h < 0)
-            _directionToFace = DirectionToFace.SW;
-        //check west
-        else if (v == 0 && h < 0)
-            _directionToFace = DirectionToFace.W;
-        //check north west
-        else if (v > 0 && h < 0)
-            _directionToFace = DirectionToFace.NW;
+        if (h != 0 || v != 0)
+            transform.rotation = Quaternion.Euler(0f, 0f, angle + 90f);
 
-        currentRot.z = (int)_directionToFace;
-        transform.localEulerAngles = currentRot;
-        
         transform.Translate(direction * _speed * Time.deltaTime, Space.World);
     }
 }
