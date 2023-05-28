5a14ad9aa8a0285e0e9b507e25db72b8b5c0c3e0
gamedevjon <jon@gamedevhq.com>
2023-05-27 15:21:35 -1000

    Working Animations

--- Assets/Game/Scripts/Hero_Controller.cs
+++ Assets/Game/Scripts/Hero_Controller.cs
@@ -9,14 +9,39 @@ public class Hero_Controller : MonoBehaviour
     [SerializeField]
     private Animator _anim;
 
+    private int _punch, _kick;
+
     private void Start()
     {
         if (_anim == null)
             Debug.LogError("Animator Component Not Assigned");
-       
     }
 
     private void Update()
+    {
+        Movement();
+
+        if (Input.GetKeyDown(KeyCode.RightControl))
+        {
+            _anim.SetTrigger("Punch_"+_punch);
+
+            _punch++;
+
+            if (_punch > 1)
+                _punch = 0;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightAlt))
+        {
+            _anim.SetTrigger("Kick_" + _kick);
+
+            _kick++;
+
+            if (_kick > 1)
+                _kick = 0;
+        }
+    }
+
+    void Movement()
     {
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
