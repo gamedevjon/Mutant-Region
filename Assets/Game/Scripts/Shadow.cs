06623dd1cc4d3e60286d7ee0c4f3fc2f83bd3b4c
gamedevjon <jon@gamedevhq.com>
2023-06-03 09:35:06 -1000

    shadow fixed

---
+++ Assets/Game/Scripts/Shadow.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shadow : MonoBehaviour
+{
+    private SpriteRenderer _shadowSprite;
+    private SpriteRenderer _parentSprite;
+    [SerializeField]
+    private Vector2 _offSet = new Vector2(-0.1f, -0.1f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _shadowSprite = GetComponent<SpriteRenderer>();
+        _parentSprite = transform.parent.GetComponent<SpriteRenderer>();
+
+        _shadowSprite.transform.localPosition = _offSet;
+        _shadowSprite.transform.localRotation = Quaternion.identity;
+
+        _shadowSprite.sprite = _parentSprite.sprite;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        _shadowSprite.transform.localPosition = _offSet;
+
+        if (_shadowSprite.sprite != _parentSprite.sprite)
+            _shadowSprite.sprite = _parentSprite.sprite;
+    }
+}
